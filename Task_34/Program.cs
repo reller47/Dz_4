5675ef6367ef46ae62dfdf2b6fd912b45d8a9489
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-05 16:45:05 +0300

    Task_34

---
+++ Task_34/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[4];
+int count = 0;
+for(int i =0; i < array.GetLength(0); i++){
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + "  ");
+}
+for(int i =0; i < array.GetLength(0); i++){
+    if((array[i] % 2) == 0){
+        count++;
+    }
+}
+Console.WriteLine("Result = " + count);
