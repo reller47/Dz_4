504fb6ba37470b72dbb2ba4b647d27cf54ed9d8a
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-05 16:54:59 +0300

    Task_36

---
+++ Task_36/Program.cs
@@ -0,0 +1,12 @@
+﻿double[] array = new double[4];
+double sum = 0;
+for(int i =0; i < array.GetLength(0); i++){
+    array[i] = new Random().Next();
+    Console.Write(array[i] + "  ");
+}
+for(int i =0; i < array.GetLength(0); i++){
+    if((i % 2) != 0){
+        sum = sum + array[i];
+    }
+}
+Console.WriteLine("Result = " + sum);
