6f729224388054afa3959c8b469f67195ab9c338
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-05 17:13:16 +0300

    Task_38

---
+++ Task_38/Program.cs
@@ -0,0 +1,14 @@
+﻿double[] array = {3.22, 1.2, 24.6, 0.8 , 9.245};
+double max = array[0];
+double min = array[0];
+for(int i = 0; i < array.GetLength(0); i++){
+    if(array[i] > max){
+        max = array[i];
+    }
+    else{
+        if(array[i] < min){
+        min = array[i];
+        }
+    }
+}
+Console.WriteLine("Result = " + (max - min));
